a2fadb9039a1b65f7e93fffb215db41976346d90
Darren Siegel <siegel.darren@gmail.com>
2020-02-04 15:04:25 -0500

    towards svn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const glob = require('glob');
+const svn = require('node-svn-ultimate');
+ 
 
-const digest = {};
+let digest = {};
 
-function main(directory) {
+function analyze(course) {
+
+  const { directory } = course;
+  digest = {};
 
   toGetXMLFiles(directory)
   .then(files => {
@@ -14,10 +19,63 @@ function main(directory) {
     .map(file => () => toInspectFile(file)))
     .then(r => {
       console.log('Outputting final digest');
-      outputDigest("digest.json");
+      outputDigest(course);
     })
 
 
+  });
+}
+
+function prepare(course) {
+  return removeWorkingDir(course)
+    .then(r => makeWorkingDir());
+}
+
+function removeWorkingDir() {
+  return spawnCustom('rm -rf ./working', {});
+}
+
+function makeWorkingDir() {
+  return spawnCustom('mkdir ./working', {});
+}
+
+
+
+function spawnCustom(command, options) {
+  const array = command.split(' ');
+  const first_command = array[0];
+  array.shift();
+  return new Promise((resolve, reject) => {
+      const spawn = require('child_process').spawn;
+
+      const child = spawn(first_command, array, options);
+      child.stdout.on('data', function(data) {
+          process.stdout.write('stdout: ' + data);
+          // Here is where the output goes
+      });
+      child.stderr.on('data', function(data) {
+          process.stdout.write('stderr: ' + data);
+          // Here is where the error output goes
+          reject(data);
+      });
+      child.on('close', function(code) {
+          process.stdout.write('closing code: ' + code);
+          // Here you can get the exit code of the script
+          resolve();
+      });
+  }); 
+}
+
+
+function checkout(course) {
+
+  const url = course.svn;
+
+  return new Promise(function(resolve, reject) {
+    svn.commands.checkout(url, './working', function( err ) {
+      console.log( "Checkout complete" );
+      resolve();
+    });
   });
 }
 
@@ -28,10 +86,14 @@ const promiseSerial = funcs =>
       func().then(Array.prototype.concat.bind(result))),
       Promise.resolve([]))
 
-function outputDigest(file) {
-  console.log(digest);
+function outputDigest(course) {
+
+  const { id, name, svn } = course;
+  digest.id = id;
+  digest.name = name;
+  digest.svn = svn;
   const data = JSON.stringify(digest, 2);
-  fs.writeFileSync(file, data);
+  fs.writeFileSync('./digests/' + id + '.json', data);
 }
 
 function toGetXMLFiles(directory) {
@@ -53,35 +115,33 @@ function toInspectFile(file) {
     parser.on('opentag', (tag, attrs) => {
       
       if (digest[tag] === undefined) {
-        console.log('adding')
         digest[tag] = { 
           tag: tag,
           count: 0,
-          attrs: {}
         };
        
       }
 
       digest[tag].count += 1;
 
-      Object.keys(attrs).forEach(a => {
+      // Object.keys(attrs).forEach(a => {
 
-        const v = attrs[a];
+      //   const v = attrs[a];
 
-        if (digest[tag].attrs[a] === undefined) {
-          digest[tag].attrs[a] = { attr: a, count: 0 };
-        }
-        if (digest[tag].attrs[a][v] === undefined) {
-          digest[tag].attrs[a][v] = {
-            value: v,
-            count: 0,
-          };
-        }
+      //   if (digest[tag].attrs[a] === undefined) {
+      //     digest[tag].attrs[a] = { attr: a, count: 0 };
+      //   }
+      //   if (digest[tag].attrs[a][v] === undefined) {
+      //     digest[tag].attrs[a][v] = {
+      //       value: v,
+      //       count: 0,
+      //     };
+      //   }
 
-        digest[tag].attrs[a].count += 1;
-        digest[tag].attrs[a][v].count += 1;
+      //   digest[tag].attrs[a].count += 1;
+      //   digest[tag].attrs[a][v].count += 1;
         
-      });
+      // });
 
     });
     parser.on('finish', () => {
@@ -97,4 +157,4 @@ function toInspectFile(file) {
   });
 }
 
-main('/Users/darrensiegel/courses/trunk');
+main({ directory: '/Users/darrensiegel/courses/trunk', id: 'test' });
